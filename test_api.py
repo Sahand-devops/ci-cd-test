a33c130d1639602ad2bc71ac2693535ca5f1d2f4
Sahand-devops <Sahandelias@live.se>
2025-11-25 17:15:21 +0100

    Add assertion for current_user_url in API test

--- test_api.py
+++ test_api.py
@@ -3,3 +3,4 @@ import requests
 def test_github_api():
     response = requests.get("https://api.github.com")
     assert response.status_code == 200
+    assert "current_user_url" in response.json()
